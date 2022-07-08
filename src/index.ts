05692828240363b351010209ac83f9e33338a3bd
suressk <sure_ssk@outlook.com>
2022-07-08 22:08:25 +0800

    feat: add suressk-eslint, remove prettier

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,12 @@
 
 /* ⬆️ after lint-staged, it will be format like this ⬇️ */
 
-const isNull = (val: unknown): val is null => {
+/* unused-vars will error */
+// const isNull = (val: unknown): val is null => {
+//   return val === null
+// }
+
+/* use '_' prefix to avoid eslint error */
+const _isNull = (val: unknown): val is null => {
   return val === null
 }
